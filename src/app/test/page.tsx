801a3d0cd9e3e1c15bcf58e38ba88289a3599199
Jack Shelton <me@jackshelton.com>
2023-03-17 21:19:29 -0500

    first commit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import styles from './test.module.css';
+
+export default function Test() {
+  return (
+    <main className={styles.main}>
+      <div>Hi there!</div>
+      <p>blueee</p>
+    </main>
+  );
+}
